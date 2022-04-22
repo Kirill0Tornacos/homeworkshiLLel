db0900f5edf0631b8ccd2d156055e453ddaa5403
Кирил <kirillmsgame@gmail.com>
2022-04-22 19:44:15 +0300

    Initial commit

---
+++ src/com/hillel/homeworks/Homework.java
@@ -0,0 +1,13 @@
+package com.hillel.homeworks;
+
+import java.io.IOException;
+
+public class Homework {
+
+    public static void main(String[] args) throws IOException {
+
+        for (int i = 0; (char) System.in.read() != 'S'; i++) {
+            System.out.println("Итерация" + i);
+        }
+    }
+}
