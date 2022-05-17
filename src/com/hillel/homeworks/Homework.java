de992b73e181fd7afd250d4ad45d40d5c1d66fa7
Кирил <kirillmsgame@gmail.com>
2022-05-17 17:55:56 +0300

    update

--- src/com/hillel/homeworks/Homework.java
+++ src/com/hillel/homeworks/Homework.java
@@ -1,13 +1,33 @@
 package com.hillel.homeworks;
 
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Homework {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number:");
 
-        for (int i = 0; (char) System.in.read() != 'S'; i++) {
-            System.out.println(i);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int result = number1 + number2;
+
+        System.out.println(result);
+
+        System.out.println("Do you want to continue or stop? press 1 - continue, press 2 - stop");
+        int num1 = scanner.nextInt();
+        int num2 = 1;
+        if (num1 == num2) {
+            System.out.println("Input number:");
+            int number3 = scanner.nextInt();
+            int number4 = scanner.nextInt();
+            int result1 = number3 + number4;
+
+            System.out.println(result1);
+
+        } else {
+            System.out.println("Your program has stopped");
         }
+
     }
 }
