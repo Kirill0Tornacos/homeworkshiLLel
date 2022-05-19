51b2dddcf461dd6817af5e50b436d2c728d40294
Кирил <kirillmsgame@gmail.com>
2022-05-19 19:36:01 +0300

    version 1.0

---
+++ src/com/hillel/homeworks/Homework2.java
@@ -0,0 +1,5 @@
+//int number2 = scanner.nextInt();
+//        int result = number1 + number2;
+//System.out.println(result);
+//if (number1 == 3) break;
+//System.out.println("if you want to stop the program write the word: Stop");
---
+++ src/com/hillel/homeworks/Main.java
@@ -0,0 +1,22 @@
+package com.hillel.homeworks;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Input number:");
+        for (int i = 0; i < 10; i++) {
+            Scanner scanner = new Scanner(System.in);
+
+            int num1 = scanner.nextInt();
+            int num2 = scanner.nextInt();
+            int result = num1 + num2;
+
+            System.out.println(result);
+
+        }
+    }
+}
+}
